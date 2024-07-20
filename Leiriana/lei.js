eb22475ed97bda2cd7f3bbd99ac797b70bc0f466
shinimori <andrea.ldu9@gmail.com>
2024-07-19 20:28:55 -0500

    Add files via upload

---
+++ Leiriana/lei.js
@@ -0,0 +1,32 @@
+ponerFoto=function(){
+    let cmpImagen;
+    cmpImagen=document.getElementById("fotoUno");
+    cmpImagen.src="lei2.jpg";
+    alert("Eres mas humana que la mayoria de personas que conozco,y eso es lo unico que importa");
+}
+
+ponerFotoDos=function(){
+    let cmpImagen;
+    cmpImagen=document.getElementById("fotoDos");
+    cmpImagen.src="lei3.jpg";
+}
+ponerFotoTres=function(){
+    let cmpImagen;
+    cmpImagen=document.getElementById("fotoTres");
+    cmpImagen.src="lei4.jpg";
+}
+ponerFotoCuatro=function(){
+    let cmpImagen;
+    cmpImagen=document.getElementById("fotoCuatro");
+    cmpImagen.src="lei5.jpg";
+}
+ponerFotoCinco=function(){
+    let cmpImagen;
+    cmpImagen=document.getElementById("fotoCinco");
+    cmpImagen.src="lei6.jpg";
+}
+ponerFotoSeis=function(){
+    let cmpImagen;
+    cmpImagen=document.getElementById("fotoSeis");
+    cmpImagen.src="leiVideo.mp4";
+}
